dad516f020e32df60bb0d5034ad9aca44d621541
ResidentMario <aleksey.bilogur@gmail.com>
2017-01-10 11:57:50 -0500

    Fiddling.

---
+++ test.js
@@ -0,0 +1,9 @@
+var d3 = require('d3');
+
+module.exports = function(n) {
+    // return d3
+    return n * 1000;
+};
+
+// d3.seThe other way aroundlect("svg").append("1");
+console.log(d3.geoPath);
